7814c2c90b8df3ab1bfc020eeba077b169951074
unknown <lkgr892@gmail.com>
2024-06-23 18:10:02 +0530

    add routes, controller. all the basic setup is done

--- src/app.js
+++ src/app.js
@@ -9,11 +9,18 @@ const corsOptions = {
 };
 
 // use method is a middleware
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-// to access and change the cookie in the users browser, this cookie can only be accessed by the server
-app.use(cookieParser());
 // used to store files publically i is exposed to the internet
 app.use(express.static("public"));
-app.use(cors(corsOptions));
+// to access and change the cookie in the users browser, this cookie can only be accessed by the server
+app.use(cookieParser());
+
+
+// routes import
+import userRoutes from "./routes/user.routes.js";
+
+// routes declaration, to use routes you will insert it in middleware
+app.use("/api/v1/users", userRoutes);
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,6 +4,16 @@ const asyncHandler = (requestHandler) => {
   };
 };
 
+/*
+Explainnation of above code:(not understood yet)
+
+const asyncHandler = (requestHandler) => (req, res, next) => { // requestHandler is a function whichis being taken as a input
+  return (req, res, next) => { // returning a function 
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err)); // calling the requestHandler/input function from parameter which has been wrapped inside a promise
+  };
+};
+*/
+
 export { asyncHandler };
 
 /*
